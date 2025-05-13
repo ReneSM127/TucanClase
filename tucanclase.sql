ea6b6e9ed3379c6722dc1c666ae3f915e24080e9
Rene Sandoval <sandovalrenemc@gmail.com>
2025-05-12 19:31:19 -0500

    Cambio de base de datos con registros

--- tucanclase.sql
+++ tucanclase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-05-2025 a las 03:10:17
+-- Tiempo de generación: 13-05-2025 a las 02:24:53
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -35,6 +35,14 @@ CREATE TABLE `inscripciones` (
   `estado` enum('Inscrito','Pendiente','Cancelado') DEFAULT 'Inscrito'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `inscripciones`
+--
+
+INSERT INTO `inscripciones` (`id`, `estudiante_id`, `tutoria_id`, `fecha_inscripcion`, `estado`) VALUES
+(1, 2, 1, '2025-05-12 19:15:54', 'Inscrito'),
+(2, 5, 2, '2025-05-12 19:16:11', 'Inscrito');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,17 @@ CREATE TABLE `materias` (
   `nombre` varchar(50) NOT NULL,
   `descripcion` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `materias`
+--
+
+INSERT INTO `materias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Matematicas', 'Mates basicas'),
+(2, 'Calculo integral', 'Integrales dobles y triples'),
+(3, 'Programacion', 'Tirar lineas'),
+(4, 'Taller de Investigacion', 'Preparate para la tesis'),
+(5, 'Redes', 'Conecta 2 switch con SSH');
 
 -- --------------------------------------------------------
 
@@ -59,6 +78,14 @@ CREATE TABLE `reviews` (
   `estrellas` tinyint(4) NOT NULL CHECK (`estrellas` between 1 and 5),
   `comentario` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `inscripcion_id`, `estrellas`, `comentario`) VALUES
+(1, 1, 4, 'Muy bueno'),
+(3, 2, 2, 'Meh');
 
 -- --------------------------------------------------------
 
@@ -79,6 +106,14 @@ CREATE TABLE `tutorias_materias` (
   `fecha_creacion` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `tutorias_materias`
+--
+
+INSERT INTO `tutorias_materias` (`id`, `tutor_id`, `materia_id`, `titulo`, `descripcion`, `duracion`, `max_estudiantes`, `precio`, `estado`, `fecha_creacion`) VALUES
+(1, 1, 2, 'Calculo avanzado papa', 'Aprende conmigo', 10, 20, 100.00, 'Programado', '2025-05-12 19:12:25'),
+(2, 3, 5, 'Puro debian', 'Aprende a las malas', 20, 3, 144.00, 'Programado', '2025-05-12 19:13:48');
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +131,17 @@ CREATE TABLE `usuarios` (
   `foto_perfil` varchar(255) DEFAULT NULL,
   `fecha_creacion` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `rol`, `descripcion`, `foto_perfil`, `fecha_creacion`) VALUES
+(1, 'rene', 'sandoval', 'rene@gmail.com', '123', 'Tutor', 'me gusta nicki Nicole', '/default', '2025-05-12 19:00:33'),
+(2, 'isabel', 'tec', 'isabel@gmail.com', '123', 'Estudiante', 'me gusta el cafe', '/default', '2025-05-12 19:01:31'),
+(3, 'omar', 'lopez', 'omar@gmail.com', '123', 'Tutor', 'me gustan las sirenas', '/default', '2025-05-12 19:02:10'),
+(4, 'carlos', 'caamal', 'caamal@gmail.com', '123', 'Tutor', 'me gusta peto', '/default', '2025-05-12 19:02:47'),
+(5, 'carlos', 'zetina', 'zetina@gmail.com', '123', 'Estudiante', 'me gustan las monas chinas', '/default', '2025-05-12 19:03:32');
 
 --
 -- Índices para tablas volcadas
@@ -146,31 +192,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `inscripciones`
 --
 ALTER TABLE `inscripciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `materias`
 --
 ALTER TABLE `materias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tutorias_materias`
 --
 ALTER TABLE `tutorias_materias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
